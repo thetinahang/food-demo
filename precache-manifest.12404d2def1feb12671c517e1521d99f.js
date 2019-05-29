c239d8a2199ce1e77ecc1b9f381cbfc6e6c0b118
thetinahang <thetinahang@gmail.com>
2019-05-28 19:57:02 -0700

    Updates

---
+++ precache-manifest.12404d2def1feb12671c517e1521d99f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acb159945c7a57e7ed5ddbc008f1583",
+    "url": "/food-demo/index.html"
+  },
+  {
+    "revision": "29a590efa1a1eba6bbc8",
+    "url": "/food-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/food-demo/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "29a590efa1a1eba6bbc8",
+    "url": "/food-demo/static/js/main.1969d77f.chunk.js"
+  },
+  {
+    "revision": "12f4932d43e2fc2c01be",
+    "url": "/food-demo/static/js/runtime~main.7e121a69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/food-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
